8568561deec79621bb75475c5341530090b80aa6
Rishabhluv <rishabhsrivastavaluv@gmail.com>
2023-05-10 12:26:57 +0530

    almostfinish

---
+++ src/component/Card.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatchCart } from './ContextReducer';
+import { useCart } from './ContextReducer';
+function Card(props) {
+    let dispatch = useDispatchCart();
+    let options = props.options;
+    let data = useCart();
+    let priceRef = useRef();
+    let priceOptions = Object.keys(options)
+    //let foodItem = props.foodItem;
+    const [qty, setQty] = useState(1);
+    const [size, setSize] = useState("");
+    let handleAddtoCart = async () => {
+        let food = [];
+        for (const item of data) {
+            if (item.id === props.foodItem._id) {
+                food = item;
+
+                break;
+            }
+        }
+        if (food !== []) {
+            if (food.size === size) {
+                await dispatch({ type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty })
+                return
+            } else if (food.size !== size) {
+                await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size });
+               return
+                //  console.log(data);
+            }
+            return
+        }
+        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size });
+    }
+    let finalPrice = qty * parseInt(options[size]);
+    useEffect(() => {
+        setSize(priceRef.current.value);
+    }, [])
+
+    return (
+        <div>
+            <div><div className="card mt-3" style={{ "width": "18rem", "maxHeight": "500px" }}>
+                <img className="card-img-top" src={props.foodItem.img} alt="Card image cap" style={{ height: "170px", objectFit: "fill" }}></img>
+                <div className="card-body">
+                    <h5 className="card-title">{props.foodItem.name}</h5>
+                    <div className='container w-10'>
+                        <select className='m-2 h-100  bg-success rounded' onChange={(e) => setQty(e.target.value)}>
+                            {
+                                Array.from(Array(6), (e, i) => {
+                                    return (
+                                        <option key={i + 1} value={i + 1}>{i + 1}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        <select className='m-2 h-100  bg-success rounded' ref={priceRef} onChange={(e) => setSize(e.target.value)}>
+                            {priceOptions.map((data) => {
+                                return <option key={data} value={data} >{data}</option>
+                            })}
+                            {/* <option value={"half"}>HALF</option>
+                            <option value={"full"}>FULL</option> */}
+                        </select>
+                        <div className='d-inline h-100 fs-5'>
+                            ₹{finalPrice}/-
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <button className="btn btn-success justify-center ms-2" onClick={handleAddtoCart}>AddtoCart</button>
+                </div>
+            </div></div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/component/Login.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+function Login() {
+    let navigate = useNavigate();
+    const [credential, setcredential] = useState({email:"",password:""})
+    const handleclick = async (e) => {
+        e.preventDefault();
+        const response = await fetch("http://localhost:5000/api/loginuser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({email:credential.email,password:credential.password})
+        })
+        const json = await response.json();
+        console.log(json);
+        if(!json.success){
+            alert("Invalid User Credential");
+        }
+        if(json.success){
+            localStorage.setItem("userEmail",credential.email);
+            localStorage.setItem("authToken",json.authtoken);
+            console.log(localStorage.getItem("authToken"))
+           navigate("/");
+        }
+
+    }
+    const onchange=(e)=>{
+        setcredential({...credential,[e.target.name]:e.target.value})
+    }
+    return (
+        <>
+            < div className='container'>
+
+                <form onSubmit={handleclick}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" name='email' value={credential.email} onChange={onchange} id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" onChange={onchange} name='password' value={credential.password}/>
+                    </div>
+                    <button type="submit" className=" m-3 btn btn-success">Submit</button>
+                    <Link to="/createuser" className='m-3 btn btn-danger'>I am a new User</Link>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Login
