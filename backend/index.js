3185fdc2c477b16f222d45e5f7b897c86ccc8421
Rishabhluv <rishabhsrivastavaluv@gmail.com>
2023-08-19 18:18:55 +0530

    build commit

--- backend/index.js
+++ backend/index.js
@@ -22,14 +22,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With ,Content-Type,Accept"
-  );
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With ,Content-Type,Accept"
+//   );
+//   next();
+// })
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../build/index.html"), function (err) {
