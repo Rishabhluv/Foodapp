5520713541410c41865199745b4639a762930eda
Rishabhluv <rishabhsrivastavaluv@gmail.com>
2023-08-19 18:23:00 +0530

    path commit

--- backend/index.js
+++ backend/index.js
@@ -39,8 +39,8 @@ app.get("/*", function (req, res) {
   });
 });
 
-app.use('/api', require("./Routes/CreateUser"));
-app.use('/api', require("./Routes/DisplayData"));
-app.use('/api', require("./Routes/OrderData"));
+app.use('/api/', require("./Routes/CreateUser"));
+app.use('/api/', require("./Routes/DisplayData"));
+app.use('/api/', require("./Routes/OrderData"));
 
 mongodb();
